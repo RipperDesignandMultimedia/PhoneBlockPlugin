f94d19cb193ff7d8e95a462b34307febec535e1e
ripperdesignandmultimedia <ripperdesignandmultimedia@users.noreply.github.com>
2014-10-13 14:14:18 +1100

    Added java file

---
+++ src/android/PhoneBlockerPlugin.java
@@ -0,0 +1,15 @@
+package com.ripperdesigns.eyesuperic;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class PhoneBlockerPlugin extends CordovaPlugin{
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	 
+	}
+}
