4ef963d182dd69d054a2d8e79e7b5939dc6c2d4f
RipperDesignandMultimedia <RipperDesignandMultimedia@users.noreply.github.com>
2014-10-14 14:42:41 +1100

    Add base phone state monitoring
    Begun code with javascript for monitoring the phone state.

--- www/phoneblock.js
+++ www/phoneblock.js
@@ -1,3 +1,28 @@
-/*
-*  This file is for the javascript inside our plugin
-*/
+/**
+ * 	PhoneBlockPlugin.js
+ * 	PhoneBlockPlugin PhoneGap plugin (Android)
+ *
+ * 	Created by Stephen Northcott of Ripper Design and Multimedia on 14/10/2014.
+ * 	Copyright 2011 Ripper Design and Multimedia. All rights reserved.
+ * 	MIT Licensed
+ *
+ */
+
+var PhoneBlockerPlugin = { 
+	start: function(successCallback, failureCallback) {
+		return cordova.exec(    
+			successCallback,
+			failureCallback,
+			'PhoneBlockerPlugin',
+			'startMonitoringPhoneState',
+			[]); // no arguments required
+	},
+	stop: function(successCallback, failureCallback) {
+		return cordova.exec(    
+			successCallback,
+			failureCallback,
+			'PhoneBlockerPlugin',
+			'stopMonitoringPhoneState',
+			[]); // no arguments required
+	}
+};
